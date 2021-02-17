b0b7cbfc42430a8d4619db2b33e7bbc7c9a08e93
Cristian Sanchez <cristian.jsan@gmail.com>
2021-02-16 20:30:48 -0400

    Commiting addition of scientia naturam plugin

---
+++ scientia-naturam-plugin/scientianaturam-custom-types.php
@@ -0,0 +1,12 @@
+/**
+ * Plugin Name:       Natural Products and Supplement Post Types
+ * Plugin URI:        scientianaturam.com
+ * Description:       This plugin adds custom post types for natural products and supplements.
+ * Version:           1.0.0
+ * Author:            Scientia Naturam
+ * Author URI:        scientianaturam.com
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       plugin-name
+ * Domain Path:       /languages
+ */
